dc14383f739aca74c62fd8e41be537ad76dc26fb
mason1999 <64181298+mason1999@users.noreply.github.com>
2024-02-26 16:30:22 +1100

    Update manage-wsl.ps1
    Removed onedrive from powershell script

--- manage-wsl.ps1
+++ manage-wsl.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$target_distribution = "Ubuntu-22.04",
-    [string]$wsl_instances_path = "C:\Users\mason\OneDrive\Desktop\wsl-instances",
+    [string]$wsl_instances_path = "C:\Users\mason\Desktop\wsl-instances",
     [string]$instance_name = "dev-environment",
 
     [Parameter(ParameterSetName = "Create")]
